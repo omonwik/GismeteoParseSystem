f43a371d67522073e68ddbe74e0b4cc6f598dc35
Artyom <fedoruschencko.artem@gmail.com>
2019-04-02 21:30:58 +0300

    simple city parse

--- GismeteoParseSystem/Program.cs
+++ GismeteoParseSystem/Program.cs
@@ -1,21 +1,41 @@
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GismeteoParseSystem
 {
-    class Program
+    sealed class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string html = null;
+            var document = new HtmlDocument();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                html = client.DownloadString("https://www.gismeteo.ru/");
+            }
+
+            document.LoadHtml(html);
+            ParseHtml(document);
+
+            Console.ReadKey();
+        }
+
+        private static void ParseHtml(HtmlDocument document)
+        {
+            if (document == null) return;
+
+            var s = document.DocumentNode.SelectSingleNode("//a[@class='link blue weather_current_link no_border']");
+            if (s == null) return;
+
+            Console.WriteLine(s.InnerText);
+            Console.ReadKey();
         }
     }
 }
